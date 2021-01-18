90e54a223e989f1f516220a56e610bb28d645ee5
prakritipathak <prakritipathak23@gmail.com>
2021-01-18 03:56:40 +0300

    Made some changes to CSS and added JS

--- src/script.js
+++ src/script.js
@@ -1,3 +1,53 @@
+       let now = new Date();
+      console.log(now);
+
+      let days = [
+        "Sun",
+        "Mon",
+        "Tues",
+        "Wed",
+        "Thurs",
+        "Fri",
+        "Sat",
+      ];
+
+      let day = days[now.getDay()];
+      console.log(day);
+
+      let year = now.getFullYear();
+      console.log(now.getFullYear());
+
+      let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+
+      let month = months[now.getMonth()];
+      console.log(month);
+
+      let date = now.getDate();
+      console.log(now.getDate());
+
+      let hours = now.getHours();
+      if (hours < 10) hours = `0${hours}`;
+      console.log(now.getHours());
+
+      let minutes = now.getMinutes();
+      if (minutes < 10) minutes = `0${minutes}`;
+      console.log(now.getMinutes());
+
+    
+      
       function searchCity(event) {
         event.preventDefault();
         let cityInput = document.querySelector("#city-input");
@@ -19,7 +69,17 @@
         console.log(response.data.main.temp);
         let temperatureElement = document.querySelector("#temperature");
         temperatureElement.innerHTML = Math.round(response.data.main.temp);
-    let iconElement = document.querySelector("icon");
-    iconElement.setAttribute()
+        let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = `${response.data.main.humidity}%`;
+    let windElement = document.querySelector("#wind");
+     windElement.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+     let descriptionElement = document.querySelector("#description");
+     descriptionElement.innerHTML = response.data.weather[0].description;
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = ` ${day}, ${month} ${date}|| ${hours}:${minutes}`;
+ }
 
-      }
+
+  
